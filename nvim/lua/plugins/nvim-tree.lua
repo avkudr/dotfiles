e048aef502c7fa249b04b67b10e75c0eb43b381e
avkudr <argon24@mail.ru>
2024-01-19 22:52:45 +0100

    Add nvim tree

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,31 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+        -- disable netrw at the very start of your init.lua
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        
+        local function my_on_attach(bufnr)
+            local api = require "nvim-tree.api"
+
+            local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+
+            -- default mappings
+            api.config.mappings.default_on_attach(bufnr)
+
+            -- custom mappings
+            vim.keymap.set('n', '<C-e>', api.tree.toggle, opts('Toggle'))
+        end
+
+        -- empty setup using defaults
+        require("nvim-tree").setup
+        {
+            on_attach = my_on_attach,
+        }
+    end,
+}
